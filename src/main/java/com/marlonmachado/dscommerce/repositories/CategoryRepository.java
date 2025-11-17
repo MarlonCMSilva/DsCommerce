2c41579c88705bb19b23065df56950024440094e
Marlon Machado <marlon.conceicao.0308@gmail.com>
2025-11-17 16:17:20 -0300

    GET categories

---
+++ src/main/java/com/marlonmachado/dscommerce/repositories/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.marlonmachado.dscommerce.repositories;
+
+import com.marlonmachado.dscommerce.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+
+
+}
---
+++ src/main/java/com/marlonmachado/dscommerce/services/CategoryService.java
@@ -0,0 +1,26 @@
+package com.marlonmachado.dscommerce.services;
+
+import com.marlonmachado.dscommerce.dto.CategoryDTO;
+import com.marlonmachado.dscommerce.entities.Category;
+import com.marlonmachado.dscommerce.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository repository;
+
+
+    @Transactional(readOnly = true)
+    public List<CategoryDTO> findAll() {
+        List<Category> result = repository.findAll();
+        return result.stream().map(x -> new CategoryDTO(x)).toList();
+    }
+
+}
